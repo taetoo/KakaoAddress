7ef58c9a42fceadcd10bef23ccecc5a6a73d34d9
taetoo <rlaougs5>
2022-08-22 16:51:23 +0900

    1차 수정

---
+++ src/main/java/com/example/kakaoaddress/KakaoApiTestVo.java
@@ -0,0 +1,24 @@
+package com.example.kakaoaddress;
+
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.List;
+
+@Data
+public class KakaoApiTestVo {
+    private HashMap<String, Object> meta;
+    private List<Documents> documents;
+}
+
+@Data
+class Documents {
+    private HashMap<String, Object> address;
+    private String address_type;
+    private Double x;
+    private Double y;
+    private String address_name;
+    private Double h_code;
+    private Double b_code;
+    private HashMap<String, Object> road_address;
+}
